48ed18581da3ead00caa62084f4644c0ecdec555
adithtoms <adithtoms@gmail.com>
2023-02-01 10:53:23 +0530

    service added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -11,32 +13,25 @@ export class LoginComponent implements OnInit {
   inputplaceholder = "Account Number"
 
 
-  // acno = '';
-  // pass = '';
+  acno = '';
+  pass = '';
 
 
-  userDetails: any = {
-    1000: { acno: 1000, username: "anu", password: "abc123", balance: 0 },
-    1001: { acno: 1001, username: "amal", password: "abc123", balance: 0 },
-    1003: { acno: 1003, username: "arun", password: "abc123", balance: 0 },
-    1004: { acno: 1004, username: "akil", password: "abc123", balance: 0 },
-
-  }
-
-  constructor() { }
+  constructor(private router:Router,private ds:DataService) { }
 
   ngOnInit(): void {
 
   }
 
-  login(a:any,b:any) {
-    var acnum = a.value
-    var psw = b.value
-    var userDetails = this.userDetails
+  login() {
+    var acnum = this.acno
+    var psw = this.pass
+    var userDetails = this.ds.userDetails
 
     if (acnum in userDetails) {
       if (psw == userDetails[acnum]["password"]) {
         alert("login success")
+        this.router.navigateByUrl('dashboard')
       } else {
         alert("incorrect password")
       }
@@ -44,34 +39,10 @@ export class LoginComponent implements OnInit {
       alert("acnym incorrect")
     }
 
-    // alert('welocome user')
+    //alert('welocome user')
   }
-  // login() {
-  //   var acnum = this.acno
-  //   var psw = this.pass
-  //   var userDetails = this.userDetails
 
-  //   if (acnum in userDetails) {
-  //     if (psw == userDetails[acnum]["password"]) {
-  //       alert("login success")
-  //     } else {
-  //       alert("incorrect password")
-  //     }
-  //   } else {
-  //     alert("acnym incorrect")
-  //   }
-
-  //   //alert('welocome user')
-  // }
-
-  // acnoChange(event: any) {
-  //   this.acno = event.target.value;
-
-  // }
-  // passChange(event: any) {
-  //   this.pass = event.target.value
-  //   console.log(this.pass);
-
-  // }
+  
+ 
 
 }
