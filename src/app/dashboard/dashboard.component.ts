b167c68f3f3f368fe87b016cccd710cd8dbff378
adithtoms <adithtoms@gmail.com>
2023-02-07 09:30:06 +0530

    dasboard added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 
 @Component({
@@ -8,10 +8,50 @@ import { DataService } from '../services/data.service';
 })
 export class DashboardComponent implements OnInit {
 
-  user:any
- constructor(private ds:DataService){
-  this.user=this.ds.currenUser
- }
+  user: any
+  acnum1: any
+  pass1: any
+  amnt1: any
+  acnum2: any
+  pass2: any
+  amnt2: any
+
+
+  constructor(private ds: DataService) {
+    this.user = this.ds.currenUser
+  }
   ngOnInit(): void {
+  }
+
+  deposit() {
+    var accnum = this.acnum1;
+    var password = this.pass1;
+    var amount = this.amnt1;
+
+
+    const result = this.ds.deposit(accnum, password, amount)
+    if (result) {
+      alert(`your account is credited with ${amount}  and balance is ${result}`)
+    } else {
+      alert("Incorrect Accountnumber or password")
+    }
+
+  }
+
+  withdraw() {
+    var accnum = this.acnum2;
+    var password = this.pass2;
+    var amount = this.amnt2;
+
+
+    const result = this.ds.withdraw(accnum, password, amount)
+    if (result) {
+      alert(`your account is debited with ${amount} and balance is ${result}`)
+
+    } else {
+      alert("Incorrect Accountnumber or password")
+
+    }
+
   }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,41 +5,94 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
 
-  currenUser:any
+  currenUser: any
+  currentAcno:any
 
   constructor() { }
 
   userDetails: any = {
-    1000: { acno: 1000, username: "anu", password: "abc123", balance: 0 },
-    1001: { acno: 1001, username: "amal", password: "abc123", balance: 0 },
-    1003: { acno: 1003, username: "arun", password: "abc123", balance: 0 },
-    1004: { acno: 1004, username: "akil", password: "abc123", balance: 0 },
+    1000: { acno: 1000, username: "anu", password: "abc123", balance: 0,transaction:[] },
+    1001: { acno: 1001, username: "amal", password: "abc123", balance: 0,transaction:[] },
+    1003: { acno: 1003, username: "arun", password: "abc123", balance: 0,transaction:[] },
+    1004: { acno: 1004, username: "akil", password: "abc123", balance: 0,transaction:[] },
 
   }
 
-register(uname:any,acno:any,psw:any){
-  if(acno in this.userDetails){
-    return false;
-  } else{
-    this.userDetails[acno]={acno,username:uname,password:psw,balance:0}
-    return true;
+  register(uname: any, acno: any, psw: any) {
+    if (acno in this.userDetails) {
+      return false;
+    } else {
+      this.userDetails[acno] = { acno, username: uname, password: psw, balance: 0 }
+      return true;
+    }
   }
-}
 
-login(acno:any,psw:any){
-  var userDetails = this.userDetails
+  login(acno: any, psw: any) {
+    var userDetails = this.userDetails
 
     if (acno in userDetails) {
       if (psw == userDetails[acno]["password"]) {
-        this.currenUser=userDetails[acno]["username"]
+        this.currenUser = userDetails[acno]["username"]
+        this.currentAcno =acno;
         console.log(this.currenUser);
-        
-       return true;
+
+        return true;
       } else {
-       return false
+        return false
       }
     } else {
-     return false
+      return false
     }
-}
+  }
+
+  deposit(acno: any, password: any, amount: any) {
+    var userDetails = this.userDetails;
+    var amt = parseInt(amount)
+    if (acno in userDetails) {
+      if (password == userDetails[acno]["password"]) {
+        userDetails[acno]["balance"] += amt;
+        // console.log(userDetails[accnum]["balance"]);
+        
+
+        userDetails[acno]["transaction"].push({Type:"Credit",Amount:amt,Balance:userDetails[acno]["balance"]})
+        
+        return userDetails[acno]["balance"];
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  withdraw(accnum: any, password: any, amount: any) {
+    var userDetails = this.userDetails;
+    var amt = parseInt(amount)
+    if (accnum in userDetails) {
+      if (password == userDetails[accnum]["password"]) {
+        if(amt<=userDetails[accnum]["balance"]){
+        userDetails[accnum]["balance"] -= amt;
+
+        userDetails[accnum]["transaction"].push({Type:"Debit",Amount:amt,Balance:userDetails[accnum]["balance"]})
+
+        console.log(userDetails);
+        
+
+        return userDetails[accnum]["balance"];
+        }else{
+          alert("insufficient balance")
+          return false;
+        }
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    } 
+  
+  }
+
+  getTransaction(acno:any){
+    return this.userDetails[acno]["transaction"]
+  }
+
 }
